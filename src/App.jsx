96bc3a27fd94853d47e1947872e207fb31b4a2c6
spindleshanks <smithd216@protonmail.com>
2023-03-13 19:45:49 +0000

    Created animated torus

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,47 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
+import * as THREE from "three";
 
 function App() {
-    const [count, setCount] = useState(0);
-
-    return (
-        <div className="App">
-            <div>
-                <p>My Portfolio</p>
-            </div>
-        </div>
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(
+        75,
+        window.innerWidth / window.innerHeight,
+        0.1,
+        1000
     );
+
+    const renderer = new THREE.WebGLRenderer({
+        canvas: document.getElementById("bg"),
+    });
+
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.position.setZ(30);
+
+    renderer.render(scene, camera);
+
+    const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+    const material = new THREE.MeshBasicMaterial({
+        color: 0xff6347,
+        wireframe: true,
+    });
+
+    const torus = new THREE.Mesh(geometry, material);
+
+    scene.add(torus);
+
+    function animate() {
+        requestAnimationFrame(animate);
+
+        torus.rotation.x += 0.001;
+        torus.rotation.y += 0.005;
+        torus.rotation.z += 0.001;
+        renderer.render(scene, camera);
+    }
+
+    animate();
+
+    return <div className="App"></div>;
 }
 
 export default App;
