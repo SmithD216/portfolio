60bb7814e6b4cb9a8a6f4fcc3d93745e4e286fff
spindleshanks <smithd216@protonmail.com>
2023-03-14 19:26:09 +0000

    Torus in space

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 function App() {
     const scene = new THREE.Scene();
@@ -21,14 +22,42 @@ function App() {
     renderer.render(scene, camera);
 
     const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
-    const material = new THREE.MeshBasicMaterial({
+    const material = new THREE.MeshStandardMaterial({
         color: 0xff6347,
-        wireframe: true,
     });
 
     const torus = new THREE.Mesh(geometry, material);
 
     scene.add(torus);
+
+    const pointLight = new THREE.PointLight(0xffffff);
+    pointLight.position.set(5, 5, 5);
+
+    const ambientLight = new THREE.AmbientLight(0xffffff);
+    scene.add(pointLight, ambientLight);
+
+    const lightHelper = new THREE.PointLightHelper(pointLight);
+    const gridHelper = new THREE.GridHelper(200, 50);
+    scene.add(lightHelper, gridHelper);
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+
+    function addStar() {
+        const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+        const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+        const star = new THREE.Mesh(geometry, material);
+
+        const [x, y, z] = Array(3)
+            .fill()
+            .map(() => THREE.MathUtils.randFloatSpread(100));
+        star.position.set(x, y, z);
+        scene.add(star);
+    }
+
+    Array(200).fill().forEach(addStar);
+
+    const spaceTexture = new THREE.TextureLoader().load("space.jpg");
+    scene.background = spaceTexture;
 
     function animate() {
         requestAnimationFrame(animate);
@@ -36,6 +65,8 @@ function App() {
         torus.rotation.x += 0.001;
         torus.rotation.y += 0.005;
         torus.rotation.z += 0.001;
+
+        controls.update();
         renderer.render(scene, camera);
     }
 
