26f76d2f60fcb76bc3e3b8a04b5210e040fa1fa7
spindleshanks <smithd216@protonmail.com>
2023-04-04 10:52:53 +0100

    Following tutorial

---
+++ src/data/portfolio.js
@@ -0,0 +1,20 @@
+export default [
+    {
+        title: "Terminal Website",
+        imgUrl: "/assets/55181940.jpg",
+        stack: ["HTML, CSS, JavaScript"],
+        link: "https://fkcodes.com",
+    },
+    {
+        title: "Developer Portfolio",
+        imgUrl: "/assets/55181940.jpg",
+        stack: ["React", "Tailwind", "Vite"],
+        link: "https://fkcodes.com",
+    },
+    {
+        title: "AI Nerf Gun",
+        imgUrl: "/assets/55181940.jpg",
+        stack: ["Python, C++, JavaScript"],
+        link: "https://fkcodes.com",
+    },
+];
---
+++ src/data/timeline.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        year: "Today",
+        title: "blah blah",
+        duration: "6 years",
+        details: "blah blah blah",
+    },
+    {
+        year: "Today",
+        title: "blah blah",
+        duration: "6 years",
+        details: "blah blah blah",
+    },
+    {
+        year: "Today",
+        title: "blah blah",
+        duration: "6 years",
+        details: "blah blah blah",
+    },
+    {
+        year: "Today",
+        title: "blah blah",
+        duration: "6 years",
+        details: "blah blah blah",
+    },
+    {
+        year: "Today",
+        title: "blah blah",
+        duration: "6 years",
+        details: "blah blah blah",
+    },
+    {
+        year: "Today",
+        title: "blah blah",
+        duration: "6 years",
+        details: "blah blah blah",
+    },
+];
